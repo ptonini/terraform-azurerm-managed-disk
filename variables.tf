858d09efdca8fb755073b03de09f8e66ccfa10ec
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-11-24 11:36:36 -0300

    chore: first commit

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "name" {}
+
+variable "rg" {}
+
+variable "storage_account_type" {}
+
+variable "create_option" {
+  default = "Empty"
+}
+
+variable "disk_size_gb" {}
+
+variable "virtual_machine_id" {
+  default = null
+}
+
+variable "virtual_machine_attachment_lun" {
+  default = null
+  type = number
+}
+
+variable "virtual_machine_attachment_caching" {
+  default = "ReadWrite"
+}
